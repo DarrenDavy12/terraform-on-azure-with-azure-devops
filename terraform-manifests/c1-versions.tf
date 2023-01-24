97117d172ba637df621d96c41bdf21030304f8ae
darrendavy <darrendavy123@gmail.com>
2023-01-24 12:25:23 +0000

    First Commit

---
+++ terraform-manifests/c1-versions.tf
@@ -0,0 +1,30 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      # version = "~> 2.0" (Use this instead in reality, but for this demo use '>= 2.0')
+      version = ">= 2.0" 
+    }
+    random = {
+      source = "hashicorp/random"
+      version = ">= 3.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = ">= 3.0"
+    }    
+  }
+# Terraform State Storage to Azure Storage Container (Values will be taken from Azure DevOps)
+  backend "azurerm" {
+    
+  }   
+}
+
+# Provider Block
+provider "azurerm" {
+ features {}          
+}
+
+
